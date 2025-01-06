739a0f604d01a8e10964efb0492cc17d35112a71
Lucie33-78 <78clg77@gmail.com>
2025-01-06 13:52:34 +0100

    Update Exo_3_streamlit.py
    Modif

--- Exo_3_streamlit.py
+++ Exo_3_streamlit.py
@@ -2,16 +2,8 @@ import streamlit as st
 from streamlit_authenticator import Authenticate
 from streamlit_option_menu import option_menu
 
-# cd "C:\Users\clegu\Desktop\DATA\2 - Exercices\Streamlit\Exo_3_streamlit"
+# cd "C:\Users\clegu\Desktop\DATA\2 - Exercices\Streamlit"
 # streamlit run Exo_3_streamlit.py
-
-# Nos données utilisateurs doivent respecter ce format
-
-# Création du menu qui va afficher les choix qui se trouvent dans la variable options
-selection = option_menu(
-            menu_title=None,
-            options = ["Accueil", "Photos"]
-        )
 
 lesDonneesDesComptes = {'usernames': {'utilisateur': {'name': 'utilisateur',
    'password': 'utilisateurMDP',
@@ -33,33 +25,36 @@ authenticator = Authenticate(
     30, # Le nombre de jours avant que le cookie expire 
 )
 
+authenticator.login() # modul d'authentification
+
 def accueil():
       st.title("Bienvenu sur ma page") # Arrivée sur la page d'accueil
       st.image("https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif") # Affiche l'image
 
-authenticator.login() # modul d'authentification
-
 if st.session_state["authentication_status"]:
-    accueil()
-    st.sidebar.title(f"Bienvenue")
     authenticator.logout("Déconnexion")  # Le bouton de déconnexion
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg")
-    with col2:
-        st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg")
-    with col3:
-        st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg")
-    add_selectbox = st.sidebar.selectbox("How would you like to be contacted?",
-                                    ("Email", "Home phone", "Mobile phone"))
-    with st.sidebar:add_radio = st.radio("Choose a shipping method",
-                                    ("Standard (5-15 days)", "Express (2-5 days)"))
+    with st.sidebar :
+         selection = option_menu(
+            menu_title="Menu",  # Titre du menu
+            options=["Accueil", "Photos"],  # Options
+            )
+    if selection == "Accueil":
+        accueil()
+    elif selection == "Photos" :
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.header("A cat")
+            st.image("https://static.streamlit.io/examples/cat.jpg")
+        with col2:
+            st.header("A dog")
+            st.image("https://static.streamlit.io/examples/dog.jpg")
+        with col3:
+            st.header("An owl")
+            st.image("https://static.streamlit.io/examples/owl.jpg")
 elif st.session_state["authentication_status"] is False:
     st.error("L'username ou le password est/sont incorrect")
 elif st.session_state["authentication_status"] is None:
     st.warning('Les champs username et mot de passe doivent être remplie')
+
 
 
